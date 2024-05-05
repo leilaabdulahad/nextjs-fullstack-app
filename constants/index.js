50006ca925d86ccd331411900d36c22f9e2f6c5f
Leila <leilaabdulahad@hotmail.com>
2024-05-05 09:32:12 +0200

    adds clerk and navbar

---
+++ constants/index.js
@@ -0,0 +1,19 @@
+
+export const headerLinks = [
+    {
+        label: "Hem",
+        route: '/'
+    },
+    {
+        label: 'Profil',
+        route: '/profile'
+    },
+    {
+        label: 'Ladda upp',
+        route: '/upload'
+    },
+    {
+        label: 'Kontakt',
+        route: '/contact'
+    }
+]
