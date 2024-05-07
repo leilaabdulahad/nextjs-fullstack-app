d7462c6d10345f2ef879b728c42d0e7150995d7c
Leila <leilaabdulahad@hotmail.com>
2024-05-07 13:06:55 +0200

    adds 'add' btn to /upload page

--- app/(root)/upload/page.jsx
+++ app/(root)/upload/page.jsx
@@ -1,9 +1,13 @@
 import TopicsList from "@/components/TopicsList";
+import Link from "next/link";
 
 export default function Upload() {
   return (
     <div>
       <h1>Upload</h1>
+      <Link className="" href={"/addTopic"}>
+          Add Topic
+            </Link>
       <TopicsList />
     </div>
   );
