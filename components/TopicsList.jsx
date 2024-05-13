455c5ee39ce65c1828d5b9c5a481fcfba911abde
Leila <leilaabdulahad@hotmail.com>
2024-05-13 15:49:33 +0200

    adds new design to list

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -22,25 +22,25 @@ export default async function TopicsList() {
   const { topics } = await getTopics();
 
   return (
-    <>
-      {topics.map((t) => (
-        <div
-          key={t._id}
-          className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
-        >
-          <div>
-            <h2 className="font-bold text-2xl">{t.title}</h2>
-            <div>{t.description}</div>
+    <div className="container mx-auto px-4">
+      <div className="grid gap-6 lg:grid-cols-2 xl:grid-cols-3 m-10">
+        {topics.map((t) => (
+          <div key={t._id} className="border border-gray-200 rounded-lg overflow-hidden">
+            <div className="p-6">
+              <h2 className="text-xl font-bold mb-2">{t.title}</h2>
+              <p className="text-gray-700">{t.description}</p>
+            </div>
+            <div className="flex items-center justify-between bg-gray-100 p-4">
+              <RemoveBtn id={t._id} />
+              <Link href={`/editTopic/${t._id}`}>
+                
+                  <HiPencilAlt className="text-gray-600" size={24} />
+                
+              </Link>
+            </div>
           </div>
-
-          <div className="flex gap-2">
-            <RemoveBtn id={t._id} />
-            <Link href={`/editTopic/${t._id}`}>
-              <HiPencilAlt size={24} />
-            </Link>
-          </div>
-        </div>
-      ))}
-    </>
+        ))}
+      </div>
+    </div>
   );
 }
