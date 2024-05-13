4993ca020cdaae93d681dd7fc24fe6b7726fe5f4
Leila <leilaabdulahad@hotmail.com>
2024-05-13 15:37:33 +0200

    adds new design to add topic page

--- app/addTopic/page.jsx
+++ app/addTopic/page.jsx
@@ -39,32 +39,34 @@ export default function AddTopic() {
   };
 
   return (
-    <>
-  
-    <form onSubmit={handleSubmit} className="flex flex-col gap-3">
-      <input
-        onChange={(e) => setTitle(e.target.value)}
-        value={title}
-        className="border border-slate-500 px-8 py-2"
-        type="text"
-        placeholder="Topic Title"
-      />
-
-      <input
-        onChange={(e) => setDescription(e.target.value)}
-        value={description}
-        className="border border-slate-500 px-8 py-2"
-        type="text"
-        placeholder="Topic Description"
-      />
-
-      <button
-        type="submit"
-        className="bg-green-600 font-bold text-white py-3 px-6 w-fit"
-      >
-        Add Topic
-      </button>
-    </form>
-    </>
+    <div className="flex justify-center items-center h-screen mt-[-150px]">
+      <form onSubmit={handleSubmit} className="w-full max-w-md p-8 bg-white rounded-lg shadow-md">
+        <h2 className="text-2xl text-black font-bold mb-4">Add Topic</h2>
+        <div className="mb-4">
+          <input
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            className="text-black border border-gray-300 px-4 py-2 w-full rounded-md focus:outline-none focus:border-green-500"
+            type="text"
+            placeholder="Topic Title"
+          />
+        </div>
+        <div className="mb-4">
+          <input
+            onChange={(e) => setDescription(e.target.value)}
+            value={description}
+            className="text-black border border-gray-300 px-4 py-2 w-full rounded-md focus:outline-none focus:border-green-500"
+            type="text"
+            placeholder="Topic Description"
+          />
+        </div>
+        <button
+          type="submit"
+          className="bg-green-600 text-white py-2 px-4 rounded-md font-semibold hover:bg-green-700 focus:outline-none focus:bg-green-700"
+        >
+          Add Topic
+        </button>
+      </form>
+    </div>
   );
 }
